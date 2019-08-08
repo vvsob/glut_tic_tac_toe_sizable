95a5cf0aac6c6bc544c9e288751155d8e9582d38
Вадим Артюшенко <artushenkomail@mail.ru>
2019-08-08 18:40:07 +0700

    win added 08.08.19

--- glut_tic_tac_toe/Source.cpp
+++ glut_tic_tac_toe/Source.cpp
@@ -1,9 +1,12 @@
 #define FIGURE_X 2
 #define FIGURE_O 1
 
-#include <stdlib.h>
+#include <cstdlib>
 #include "glut.h"
 #include <cmath>
+
+#include <Windows.h>
+#include <string>
 
 //cursor position
 int curs_x;
@@ -17,8 +20,11 @@ int border_indent = 50;
 int frame_step = (win_x_size - border_indent * 2) / 3;
 
 bool move = 1;
+bool gameOver = 0;
 
 short int table[3][3];
+
+bool linePoints[3][3];
 
 void changeSize(int w, int h) {
 	// предупредим деление на ноль
@@ -72,11 +78,73 @@ void drawO(int x, int y) {
 	glColor3f(1, 1, 1);
 }
 
+void drawLine() {
+	bool temp = 0;
+	glColor3f(0, 1, 0);
+	glLineWidth(5);
+	glBegin(GL_LINES);
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++) {
+			if (linePoints[j][i]) {
+				glVertex2f(j * frame_step + border_indent + frame_step / 2,
+					i * frame_step + border_indent + frame_step / 2);
+				if (temp == 1)
+				{
+					//glEnd();
+					//Sleep(3000);
+					gameOver = 1;
+				}
+				temp = 1;
+			}
+		}
+	glEnd();
+	glColor3f(1, 1, 1);
+	glLineWidth(1);
+
+}
+
+
+short int winCheck() {
+
+	for (int i = 0; i < 3; i++) {
+		if (table[0][i] == table[1][i] && table[1][i] == table[2][i] && table[0][i] > 0) {
+			linePoints[0][i] = 1;
+			linePoints[2][i] = 1;
+			return table[0][i];
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		if (table[i][0] == table[i][1] && table[i][1] == table[i][2] && table[i][0] > 0) {
+			linePoints[i][0] = 1;
+			linePoints[i][2] = 1;
+			return table[i][0];
+		}
+	}
+
+	if (table[0][0] == table[1][1] && table[1][1] == table[2][2] && table[0][0] > 0) {
+		linePoints[0][0] = 1;
+		linePoints[2][2] = 1;
+		return table[0][0];
+	}
+	if (table[0][2] == table[1][1] && table[1][1] == table[2][0] && table[0][2] > 0) {
+		linePoints[0][2] = 1;
+		linePoints[2][0] = 1;
+		return table[0][2];
+	}
+
+	return 0;
+}
+
 void clickCheck(int ind_x, int ind_y) {
-	if (table[ind_x][ind_y] < 1) {
+	if (table[ind_x][ind_y] < 1 ) {
 		table[ind_x][ind_y] = int(move) + 1;
 		move = !move;
 	}
+	winCheck();
+}
+
+void yesNoClickCheck(int x, int y) {
+
 }
 
 void drawFigs() {
@@ -93,50 +161,53 @@ void drawFigs() {
 	glLineWidth(1);
 }
 
-void winCheck() {
-	short int win = 0;
 
-	for (int i = 0; i < 3; i++) {
-		if (table[0][i] == table[1][i] && table[1][i] == table[2][i])
-			win = table[0][i];
-	}
-	for (int i = 0; i < 3; i++) {
-		if (table[i][0] == table[i][1] && table[i][1] == table[i][2])
-			win = table[i][0];
-	}
-
-	if (table[0][0] == table[1][1] && table[1][1] == table[2][2])
-		win = table[0][0];
-	if (table[0][2] == table[1][1] && table[1][1] == table[2][0])
-		win = table[0][2];
-}
 
 void RenderScene() {
+	if (!gameOver) {
+		glBegin(GL_LINE_LOOP); //frame
+		glVertex2f(border_indent, border_indent);
+		glVertex2f(win_x_size - border_indent, border_indent);
+		glVertex2f(win_x_size - border_indent, win_y_size - border_indent);
+		glVertex2f(border_indent, win_y_size - border_indent);
+		glEnd();
 
-	glBegin(GL_LINE_LOOP); //frame
-	glVertex2f(border_indent, border_indent);
-	glVertex2f(win_x_size - border_indent, border_indent);
-	glVertex2f(win_x_size - border_indent, win_y_size - border_indent);
-	glVertex2f(border_indent, win_y_size - border_indent);
-	glEnd();
+		glBegin(GL_LINES);
+		glVertex2f(border_indent + frame_step, border_indent);
+		glVertex2f(border_indent + frame_step, win_y_size - border_indent);
+		glVertex2f(border_indent + frame_step * 2, border_indent);
+		glVertex2f(border_indent + frame_step * 2, win_y_size - border_indent);
 
-	glBegin(GL_LINES);
-	glVertex2f(border_indent + frame_step, border_indent);
-	glVertex2f(border_indent + frame_step, win_y_size - border_indent);
-	glVertex2f(border_indent + frame_step * 2, border_indent);
-	glVertex2f(border_indent + frame_step * 2, win_y_size - border_indent);
+		glVertex2f(border_indent, border_indent + frame_step);
+		glVertex2f(win_x_size - border_indent, border_indent + frame_step);
+		glVertex2f(border_indent, border_indent + frame_step * 2);
+		glVertex2f(win_x_size - border_indent, border_indent + frame_step * 2);
+		glEnd();
 
-	glVertex2f(border_indent, border_indent + frame_step);
-	glVertex2f(win_x_size - border_indent, border_indent + frame_step);
-	glVertex2f(border_indent, border_indent + frame_step * 2);
-	glVertex2f(win_x_size - border_indent, border_indent + frame_step * 2);
-	glEnd();
+		drawFigs();
 
-	//glBegin(GL_POINTS);
-	//glVertex2f(curs_x - 5, curs_y - 5);
-	//glEnd();
-
-	drawFigs();
+		drawLine();
+		if (gameOver) { 
+			glutSwapBuffers();
+			Sleep(1500); 
+		}
+	} else {
+		glClear(GL_COLOR_BUFFER_BIT);
+		//glBegin(GL_POINTS);
+		//glVertex2f(curs_x - 5, curs_y - 5);
+		//glEnd();
+		int x1 = win_x_size / 6 - glutBitmapWidth(GLUT_BITMAP_TIMES_ROMAN_24, '0') * 1.5 + win_x_size / 3 * 1;
+		//char* scr = new char[4];
+		//memset(scr, '\0', 3);
+		char msg[] = "WIN\0";
+		//char *msg = new char[50];
+		//strcpy_s(msg)
+		for (char* c = msg; *c != '\0'; c++) {
+			glRasterPos2f(x1, 30);
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c);
+			x1 += glutBitmapWidth(GLUT_BITMAP_TIMES_ROMAN_24, *c) + 5;
+		}
+	}
 
 	glutSwapBuffers();
 }
@@ -150,10 +221,16 @@ void MouseClick(int button, int state, int width, int height) {
 	curs_y = height;
 	int ind_x = (curs_x - border_indent) / frame_step;
 	int ind_y = (curs_y - border_indent) / frame_step;
-	clickCheck(ind_x, ind_y);
+	if (!gameOver)
+		clickCheck(ind_x, ind_y);
+	else
+		yesNoClickCheck(curs_x, curs_y);
 }
 
 int main(int argc, char** argv) {
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			linePoints[i][j] = 0;
 	glutInit(&argc, argv);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(win_x_size, win_y_size);
